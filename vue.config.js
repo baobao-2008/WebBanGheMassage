95515a0913514651dbab0ab8c5396de8e4e0cddf
baobao-2008 <baovnqth06015@gmail.com>
2026-02-28 10:26:19 +0700

    `deploy`

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/WebBanGheMassage/" : "/",
+  publicPath: "./",
 
   transpileDependencies: true,
   lintOnSave: false,
